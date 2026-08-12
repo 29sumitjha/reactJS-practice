e7ca0255228d670bbb6fa5dc99e9588c9a54bb60
Sumit Kumar Jha <sumitjha5335@gmail.com>
2026-08-12 23:01:50 +0530

    Replace href attribute from Links and add to property

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -56,7 +56,7 @@ export function OrdersPage(){
                             </div>
 
                             <div className="product-actions">
-                            <Link href="/tracking">
+                            <Link to="/tracking">
                                 <button className="track-package-button button-secondary">
                                 Track package
                                 </button>
@@ -84,7 +84,7 @@ export function OrdersPage(){
                             </div>
 
                             <div className="product-actions">
-                            <Link href="/tracking">
+                            <Link to="/tracking">
                                 <button className="track-package-button button-secondary">
                                 Track package
                                 </button>
@@ -135,7 +135,7 @@ export function OrdersPage(){
                             </div>
 
                             <div className="product-actions">
-                            <Link href="/tracking">
+                            <Link to="/tracking">
                                 <button className="track-package-button button-secondary">
                                 Track package
                                 </button>
