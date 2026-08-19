437875474974af1fd8f2507ffdd96a95fd415e09
Sumit Kumar Jha <sumitjha5335@gmail.com>
2026-08-19 21:37:06 +0530

    Make Place Order button work

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -53,7 +53,7 @@ export function CheckoutPage({ cart , loadCart}){
 
                     <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart}/>
 
-                    <PaymentSummary paymentSummary={paymentSummary}/>
+                    <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart}/>
                 </div>
             </div>
         </>
