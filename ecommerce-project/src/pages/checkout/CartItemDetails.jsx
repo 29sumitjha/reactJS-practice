c22bce4da7d9bb7cb22035ee5fc920260325d7b1
Sumit Kumar Jha <sumitjha5335@gmail.com>
2026-08-19 17:06:24 +0530

    Create update delivery option feature

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,7 +1,6 @@
 import { formatMoney } from "../../utils/money";
 
 export function CartItemDetails({cartItem}){
-    console.log(cartItem);
     return (
         <>
             <img className="product-image"
