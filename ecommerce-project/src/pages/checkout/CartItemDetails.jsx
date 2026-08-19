e8a46dc470d86ab23d1ecbb049ae4e39120c0212
Sumit Kumar Jha <sumitjha5335@gmail.com>
2026-08-19 16:17:58 +0530

    Create add to cart feature

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,6 +1,7 @@
 import { formatMoney } from "../../utils/money";
 
 export function CartItemDetails({cartItem}){
+    console.log(cartItem);
     return (
         <>
             <img className="product-image"
@@ -15,7 +16,7 @@ export function CartItemDetails({cartItem}){
                 </div>
                 <div className="product-quantity">
                     <span>
-                        Quantity: <span className="quantity-label">2</span>
+                        Quantity: <span className="quantity-label">{cartItem.quantity}</span>
                     </span>
                     <span className="update-quantity-link link-primary">
                         Update
