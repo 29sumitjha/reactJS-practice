63bb102ebe90ee048a8062a5e1cde229627ce9db
Sumit Kumar Jha <sumitjha5335@gmail.com>
2026-08-20 15:53:59 +0530

    Make search bar work

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router';
 import { Header } from '../../components/Header';
 import { ProductsGrid } from './ProductsGrid';
 import './HomePage.css';
@@ -8,7 +9,8 @@ import './HomePage.css';
 export function HomePage({ cart, loadCart }){
 
     const [products, setProducts] = useState([]);
-    
+    const [searchParams] = useSearchParams();
+    const search = searchParams.get('search');
 
     // fetch('http://localhost:3000/api/products')
     //     .then((response)=>{
@@ -26,13 +28,14 @@ export function HomePage({ cart, loadCart }){
         // ;
 
         const getHomeData = async ()=>{
-            const response = await axios.get('/api/products');
+            const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+            const response = await axios.get(urlPath);
             setProducts(response.data);
         };
 
         getHomeData();
         
-    }, []);
+    }, [search]);
 
     
     
