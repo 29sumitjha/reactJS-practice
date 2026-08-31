d66cce9fa7585df0eb1fdf87c8777c17cb79aac9
Sumit Kumar Jha <sumitjha5335@gmail.com>
2026-08-31 17:09:00 +0530

    Add test for the Homepage to check if the Add to cart buttons work

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -1,6 +1,7 @@
 import { beforeEach, describe, it, vi, expect } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import { HomePage } from "./HomePage";
 
@@ -9,6 +10,7 @@ vi.mock('axios');
 describe('HomePage component', ()=>{
 
     let loadCart;
+    let user;
 
     beforeEach(()=>{
         loadCart = vi.fn();
@@ -41,6 +43,7 @@ describe('HomePage component', ()=>{
                 };
             }
         });
+        user = userEvent.setup();
     });
 
     it('displays the products correctly', async ()=>{
@@ -61,5 +64,33 @@ describe('HomePage component', ()=>{
         expect(
             within(productContainers[1]).getByText('Intermediate Size Basketball')
         ).toBeInTheDocument(); 
+    });
+
+    it("adds product to the cart", async ()=>{
+
+        render(
+            <MemoryRouter>
+                <HomePage cart= {[]} loadCart={loadCart}/>
+            </MemoryRouter>
+        );
+
+        const productContainers = await screen.findAllByTestId('product-container');
+
+        const addToCartButton1 = within(productContainers[0]).getByTestId('add-to-cart-button');
+        await user.click(addToCartButton1);
+
+        const addToCartButton2 = within(productContainers[1]).getByTestId('add-to-cart-button');
+        await user.click(addToCartButton2);
+
+        expect(axios.post).toHaveBeenNthCalledWith(1, '/api/cart-items', {
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 1
+        });
+        expect(axios.post).toHaveBeenNthCalledWith(2, '/api/cart-items', {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1
+        });
+
+        expect(loadCart).toHaveBeenCalledTimes(2);
     });
 });
